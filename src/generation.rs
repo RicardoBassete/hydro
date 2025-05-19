3db970966a681a429437ccaf17e6088157af5eaf
Ricardo Bassete <ricardogbassete@gmail.com>
2025-05-19 11:58:54 -0300

    add exit_code var

--- src/generation.rs
+++ src/generation.rs
@@ -10,12 +10,11 @@ impl Generator {
     }
 
     pub fn generate(&self) -> String {
+        let exit_code = self.root.expr.int_lit.value.clone().unwrap();
+
         let mut result = String::from("global _start\n_start:\n");
         result.push_str("    mov rax, 60\n");
-        result.push_str(&format!(
-            "    mov rdi, {}\n",
-            self.root.expr.int_lit.value.clone().unwrap()
-        ));
+        result.push_str(&format!("    mov rdi, {}\n", exit_code));
         result.push_str("    syscall");
 
         result
